27be64bb8c3568bb0213cd9fd73c25d685d9d0fc
root <root@whg-ubuntu-1>
2018-07-11 17:04:17 +0800

    complete netinfo.py & bronze_lib/bronze_net.py.

--- src/bronze_lib/bronze_net.py
+++ src/bronze_lib/bronze_net.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+from time import sleep
 
 #This file is a part of project Bronze.
 
 '''
-
-
-
+class Net_card()  ： 定义Net_card类
+        nic_addr    :   IP地址
+        nic_stats   ：  接口状态
+        nic_mac     ：  MAC地址及PCI地址
+        nic_attr    ：  网卡属性及接口名
+        utilization(interval=5,period=2): 返回平均值 {网卡名：（接收速率，发送速率）}
 '''
 
 import os,re
 import psutil
+import time
 from subprocess import Popen, PIPE
 
 
@@ -19,9 +24,7 @@ class Net_card(object):
         self.nic_addr={}
         self.nic_stats={}
         self.nic_mac={}
-        self.__nic_attr=[]
         self.nic_attr={}
-
         
         self.__get()
         return
@@ -29,23 +32,20 @@ class Net_card(object):
     def __get(self):
         self.nic_addr=psutil.net_if_addrs()
         self.nic_stats=psutil.net_if_stats()
-        self.__nic_attr=self.__get_nic_dev()
+        self.nic_attr=self.__get_nic_dev()
         self.nic_mac=self.__get_nic_mac()    
 
-        for i in self.__nic_attr:
+        for i in self.nic_attr.keys():
             for j in self.nic_mac.keys():
-                if i[0] == self.nic_mac[j][0]:
-                    self.nic_attr[j]=i
-
-
-
+                if i == self.nic_mac[j][0]:
+                    self.nic_attr[i].insert(0,j)
 
         return
 
     def __get_nic_dev(self):
         #use lspci -Dmm get pci info
         p=Popen('lspci -Dmm',shell=True,stdout=PIPE, stderr=PIPE).stdout.read().split(b"\n")
-        nic_attr=[]
+        nic_attr={}
         for k,v in enumerate(p):
             a=v.decode("ascii")
             re_pci_addr=r'\d{4}:\d{2}:\d{2}.\d'
@@ -61,14 +61,15 @@ class Net_card(object):
                 tmp_pci_rev=re.findall(re_pci_rev,a)
 
                 if tmp_pci_attr[0]=="Ethernet controller" :
-                    nic_attr.append((tmp_pci_addr[0],tuple(tmp_pci_attr),tmp_pci_rev[0]))
+                    #nic_attr.append([tmp_pci_addr[0],tmp_pci_attr,tmp_pci_rev[0]])
+                    nic_attr[tmp_pci_addr[0]]=[tmp_pci_attr,tmp_pci_rev[0]]
         return nic_attr
 
     def __get_nic_mac(self):
         #use /sys/bus/pci/devices/0000:02:01.0/net/ens33/address to find nic name & mac addrsss
         nic_mac={}
-        for v in self.__nic_attr:
-            tmp_path_1="/sys/bus/pci/devices/%s/net/" % v[0]
+        for v in self.nic_attr.keys():
+            tmp_path_1="/sys/bus/pci/devices/%s/net/" % v
             os.chdir(tmp_path_1)
             tmp_name=[x for x in os.listdir(".") if os.path.isdir(x)]
             tmp_path_2=os.path.join(tmp_path_1,tmp_name[0])
@@ -76,23 +77,54 @@ class Net_card(object):
             with open(file_path,'rt',encoding='utf-8',errors='ignore') as mf:
                 tmp_mac=mf.read().strip("\n")
                 mf.close()
-            nic_mac[tmp_name[0]]=(v[0],tmp_mac)
+            nic_mac[tmp_name[0]]=(v,tmp_mac)
 
         return nic_mac
 
+    def utilization(self,interval=5,period=2):
+        #周期抓取信息
+        trdata=[]
+        i=0
+        #print("NIC{:5s} : {:5s} re/bytes {:10s} tr/bytes".format("","",""))
+        while i < interval:
+            i = i+1
+            tmp_trdata=self.__get_net_tr()
+            trdata.append(tmp_trdata)
+            #for k,v in enumerate(tmp_trdata.items()):
+                #print("{:8s} :     {:20s} {}".format(v[0],v[1][0],v[1][1]))
+            if i < interval:
+                sleep(period)
+        #处理抓取的信息，分解出接收/发送/时间戳，用于后续处理
+        util={}
+        recive={}
+        tran={}
+        ptime={}
+        for k,v in enumerate(trdata[0].keys()):
+            recive[v]=int(trdata[interval-1][v][0])-int(trdata[0][v][0])
+            tran[v]=int(trdata[interval-1][v][1])-int(trdata[0][v][1])
+            ptime[v]=trdata[interval-1][v][2]-trdata[0][v][2]
+            if ptime[v] == 0:
+                #避免时间周期为 0
+                print("Time is zero.")
+                exit()
+        #计算时间周期内B/s的值
+        for k,v in  enumerate(recive.keys()):
+            util_re=recive[v]/ptime[v]
+            util_tr=tran[v]/ptime[v]
+            util[v]=(util_re,util_tr)
+        #返回平均值 {网卡名：（接收速率，发送速率）}
+        return util
 
-
-
-
-
-a=Net_card()
-
-for k,v in enumerate(a.__dict__.items()):
-    print(k,v)
-#print(a.nic_stats)
-#print(psutil.net_connections())
-
-#print(a.nic_name)
+    def __get_net_tr(self):
+        trdata={}
+        with open('/proc/net/dev','tr',encoding='utf-8',errors='ignore') as myfile:
+            for line in myfile.readlines():
+                dl=line.strip().split(":")
+                if dl[0].strip() in self.nic_mac.keys():
+                    dd=dl[1].split()
+                    trdata[dl[0]]=(dd[0],dd[8],int(time.time()))
+            myfile.close()
+        return trdata
 
 
 
--- src/netinfo.py
+++ src/netinfo.py
@@ -13,6 +13,130 @@ import bronze_lib.bronze_net as net
 
 
 
+def main(argv):
+    log_file = ''
+    show_argv = ''
+    
+    try:
+        opts, args = getopt.getopt(argv,"hs:",["help","show="])
+    except getopt.GetoptError:
+        print("wrong args!")
+        __usage()
+        exit()
+
+    #命令行无参数处理
+    if opts==[] :
+        print("no args!")
+        __show_info()
+        exit()
+    else:
+        #命令行参数处理
+        for opt, arg in opts:
+            if opt in ("-h", "--help"):
+                __usage()
+                exit()
+            elif opt in ("-s", "--show"):
+                __show_info(arg)
+                exit()
+    return
+
+
+def __usage():
+    usage_content=\
+'''
+usage netinfo.py [-hs | --help --show <interface|utilization>]
+
+--help 打印帮助信息
+无参数/-s/--show 只显示信息，不保存
+    interface  显示网卡信息
+    utilization   显示流量信息
+'''
+    print(usage_content)
+    return
+
+def __show_info(st=""):
+    util_content=\
+'''PORT : {}
+    Rx    : {}
+    Tx    : {}
+'''
+    if st == "" or st.upper() == "INTERFACE" :
+        __show_ifinifo()
+    elif st.upper() == "UTILIZATION" :
+        mynic=net.Net_card()
+        for k,v in enumerate(mynic.utilization().items()):
+            print(util_content.format(v[0],v[1][0],v[1][1]))
+    return
+
+def __show_ifinifo():
+    
+    mynic=net.Net_card()
+    nic_content=\
+'''
+Nic  : {}
+    Pci_addr : {}
+    Vendor   : {}
+    Model    : {}
+    S_vendor : {}
+    S_model  : {}
+    Rev      : {}
+'''
+    ip_content=\
+'''        IP      : 
+            ip_addr  : {}
+            net_mask : {}
+'''
+    port_content=\
+'''    PORT  ： {}
+{}        STATS   :
+            isup     : {}
+            duplex   : {}
+            speed    : {} Mb/s
+            mtu      : {} Bytes
+        MAC     :
+            mac_addr : {}
+        PCI     :
+            pci_addr : {}
+'''
+    for k,v in enumerate(mynic.nic_attr.items()):
+        pci_addr=v[0]
+        vendor=v[1][1][1]
+        model=v[1][1][2]
+        s_vendor=v[1][1][3]
+        s_model=v[1][1][4]
+        rev=v[1][2][2:]
+        nic_name="%s %s" % (s_vendor,s_model)
+        print(nic_content.format(nic_name,pci_addr,vendor,model,s_vendor,s_model,rev))
+        for k1,v1 in enumerate(mynic.nic_mac.items()):
+            if v[0] == v1[1][0]:
+                port_name=v1[0]
+                str_port_ip=""
+                #处理IP地址，考虑多IP地址情况
+                for k2,v2 in enumerate(mynic.nic_addr[port_name]):
+                    port_ip_addr=""
+                    port_net_mask=""
+                    #ipv4 & ipv6 addr
+                    if str(v2[0])=="AddressFamily.AF_INET" or str(v2[0])=="AddressFamily.AF_INET6":
+                        port_ip_addr=v2[1]
+                        port_net_mask=v2[2]
+                        str_port_ip += ip_content.format(port_ip_addr,port_net_mask)
+                #处理 nic stats
+                port_stats=mynic.nic_stats[port_name]
+                port_isup=port_stats[0]
+                port_duplex=str(port_stats[1]).split(".")[1]
+                port_speed=port_stats[2]
+                port_mtu=port_stats[3]
+                #处理nic mac
+                port_mac=mynic.nic_mac[port_name][1]
+                #处理PCI addr
+                port_pci_addr=mynic.nic_mac[port_name][0]
+                print(port_content.format(port_name,str_port_ip,port_isup,port_duplex,port_speed,port_mtu,port_mac,port_pci_addr))
+    return
+
+
+if __name__ == "__main__" :
+    check_platform(PLATFORM) #监测运行平台是否符合要求
+    main(sys.argv[1:])
 
 
 
